4b2cd4e41f13e9a4aae31b37b5a787ed38223340
Emily Weyda <54867682+weydaej@users.noreply.github.com>
2020-05-06 21:09:35 -0400

    adding corpora and regex

---
+++ corpora-and-regex.py
@@ -0,0 +1,8 @@
+# Corpora and Regex
+import nltk
+import os
+import sys
+# nltk.download('brown')
+from nltk.corpus import brown # Brown University Stanford Corpus of Present-Dat American English
+
+x = nltk.data.load('big.txt', format='raw')
