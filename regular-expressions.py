0b429cac8b0dcf1ccd492a16ea1f54d33c83e99e
Emily Weyda <54867682+weydaej@users.noreply.github.com>
2020-05-08 05:08:00 -0400

    starting re key

--- regular-expressions.py
+++ regular-expressions.py
@@ -1,6 +1,42 @@
 # documentation: https://docs.python.org/3/library/re.html
 
 # '.' = matches any char except for new line. (DOTALL flag will match any char incl. new line)
-# '^' = 
+# '^' = matches the start of the string, and in MULTILINE mode also matches immediately after each new line
+# '$' =
+# '*' =
+# '+' =
+# '?' =
+# '*?', '+?', '??' =
+# '{m}' =
+# '{m,n}?' =
+# '\' =
+# '[]' =
+# '|' =
+# '(...)' =
+# '(?...)' =
+# '?aiLmsux' =
+# '(?:...)' =
+# '(?aiLmsux-imsx:...)' =
+# '(?P<name>...)' =
+# '(?P=name)' =
+# '(?#...)' =
+# '(?=...)' =
+# '(?!...)' =
+# '(?<=...)' =
+# '(?<!...)' =
+# '(?(id/name)yes-pattern|no-pattern)' =
+# '\number' =
+# '\A' =
+# '\b' =
+# '\B' =
+# '\d' =
+# '\D' =
+# '\s' =
+# '\S' =
+# '\w' =
+# '\W' =
+# '\Z' =
+# '\u', '\U'. '\N' =
+
 import re
 
