c3fe1c95e2f2fac984b73b5c9c069694146d1f2d
Emily Weyda <54867682+weydaej@users.noreply.github.com>
2020-05-16 19:19:28 -0400

    fixed nltk issue

--- pos_taggers.py
+++ pos_taggers.py
@@ -2,3 +2,12 @@
 # module load python/3.5
 # brown and treebank corpora
 
+from nltk.corpus import treebank
+train_sents = treebank.tagged_sents()[:3000]
+test_sents = treebank.tagged_sents()[3000:]
+
+# my first tagger
+from nltk.tag import DefaultTagger
+tagger = DefaultTagger('NN')
+print(tagger.tag_sents([['Hello','.'],['My','name','is','Steve']]))
+print(tagger.evaluate(test_sents))
