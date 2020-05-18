3b01e7c2e7441663eb4e734ceab05d37bebf3f9f
Emily Weyda <54867682+weydaej@users.noreply.github.com>
2020-05-17 20:19:52 -0400

    adding unigrams, bigrams and trigrams

--- pos_taggers.py
+++ pos_taggers.py
@@ -11,3 +11,22 @@ from nltk.tag import DefaultTagger
 tagger = DefaultTagger('NN')
 print(tagger.tag_sents([['Hello','.'],['My','name','is','Steve']]))
 print(tagger.evaluate(test_sents))
+
+# unigrams
+from nltk.tag import UnigramTagger
+unigram_tagger = UnigramTagger(train_sents)
+tagger = UnigramTagger(train_sents, cutoff=3)
+print(tagger.evaluate(test_sents))
+
+# bigrams
+from nltk.tag import BigramTagger
+bigram_tagger = BigramTagger(train_sents)
+tagger = BigramTagger(train_sents, cutoff=3)
+print(tagger.evaluate(test_sents))
+
+# trigrams
+from nltk.tag import TrigramTagger
+trigram_tagger = TrigramTagger(train_sents)
+tagger = TrigramTagger(train_sents, cutoff=3)
+print(tagger.evaluate(test_sents))
+
